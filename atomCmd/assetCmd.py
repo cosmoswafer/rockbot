d2aff8bc21d0b5f406a666bdd5b3031ff1f7e93c
fong <fong@jupiter-atom>
2022-11-06 21:45:00 +0800

    Added sub parser implementation

--- atomCmd/assetCmd.py
+++ atomCmd/assetCmd.py
@@ -14,19 +14,25 @@ class assetCmd(cmd):
     def _initParser(self, parser):
         self.parser = parser
 
-        self.parser.add_argument("--date" \
+        self.subparser = self.parser.add_subparsers(dest="asset_cmd", help="Asset sub commands")
+        #self.subparser.required = True
+        self.add_parser = self.subparser.add_paser("add", help="Add new asset")
+        self.add_parser.add_argument("--date" \
                 , type=datetime.date.fromisoformat \
                 , default=datetime.date.today().isoformat() \
                 , help="日期，ISO format, i.e. YYYY-MM-DD" )
-        self.parser.add_argument("--item", dest="item", type=str, help="內容")
-        self.parser.add_argument("--price", dest="price", type=float, help="價錢")
-        self.parser.add_argument("--period", dest="period", type=float, help="Deprecation period, in years")
-        self.parser.add_argument("--acct", dest="acct", choices=["A","B","C"], help="Main account")
-        self.parser.add_argument("--cat", dest="cat", type=str, help="Catagory")
+        self.add_parser.add_argument("--item", dest="item", type=str, help="內容")
+        self.add_parser.add_argument("--price", dest="price", type=float, help="價錢")
+        self.add_parser.add_argument("--period", dest="period", type=float, help="Deprecation period, in years")
+        self.add_parser.add_argument("--acct", dest="acct", choices=["A","B","C"], help="Main account")
+        self.add_parser.add_argument("--cat", dest="cat", type=str, help="Catagory")
+
+        self.delete_parser = self.subparser.add_parser("delete", help="Delete existing asset")
+        self.delte_parser.add_argument("asset_id", type=int, help="Asset id to be deleted")
+
+
         #self.parser.add_argument("--report", action="store_true", help="顯示報告")
         group = self.parser.add_mutually_exclusive_group()
-        group.add_argument("--add", action="store_true", default=True)
-        group.add_argument("--delete", action="store_true")
 
         self.parser.set_defaults(func=self.update)
 
@@ -40,6 +46,7 @@ class assetCmd(cmd):
         await bot.reply(str(await self._asset(bot.args)))
 
     async def _asset(self, args):
+        """
         e = {
             "name": args.item,
             "price": args.price,
@@ -47,5 +54,6 @@ class assetCmd(cmd):
             "account": args.acct,
             "catagory": args.cat
         }
-        return e
+        """
+        return str(args)
 
