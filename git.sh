050ee23540ef0936e0ad947e8708440f98815381
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2023-11-24 14:56:12 +0800

    Clean up

--- git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#sudo -u nginx git config user.email "nginx@atom.local"
-#sudo -u nginx git config user.name "Nginx"
-
-sqlite3 MQ-7860.db .dump > MQ-7860.sql
-git commit -am "Table dump"
