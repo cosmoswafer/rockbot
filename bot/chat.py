a6fadd579a1b50ffd708477a9655bf55167c29c2
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2024-01-10 15:31:23 +0800

    Add command to clear chat history

--- bot/chat.py
+++ bot/chat.py
@@ -8,11 +8,23 @@ from bot.openai import OpenAi
 class chatBot:
     def __init__(self, openai=OpenAi()):
         self.openai = openai
+        self.commands = {"clear": self.clear}
+
+    async def clear(self, bot, room_id):
+        self.openai.histories[room_id] = []
+        await bot.reply("History cleared")
 
     async def chat(self, bot):
         if conf.debug:
             print(f"chatBot incoming message: [{bot.rid}]{bot.msg}")
-        asyncio.create_task(self._query(bot, bot.rid, bot.msg))
+
+        # Map commands
+        if bot.msg.startswith("!"):
+            command = bot.msg[1:].split(" ")[0]
+            if command in self.commands:
+                await self.commands[command](bot, bot.rid)
+        else:
+            asyncio.create_task(self._query(bot, bot.rid, bot.msg))
 
     async def _query(self, bot, rid, msg) -> None:
         await bot.reply(await self.openai.submit(rid, msg))
